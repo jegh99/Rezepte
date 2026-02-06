268e8cf751dc934af4bb5e178b82d4a600edff05
jegh99 <jan.gohlke2@web.de>
2026-02-06 15:17:49 +0100

    CMakeLists.txt implemented.

---
+++ build/CMakeFiles/Rezepte.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../executables/Rezepte"
+  "../executables/Rezepte.pdb"
+  "CMakeFiles/Rezepte.dir/sources/application/main.c.o"
+  "CMakeFiles/Rezepte.dir/sources/application/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Rezepte.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Rezepte.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Rezepte.
+# This may be replaced when dependencies are built.
