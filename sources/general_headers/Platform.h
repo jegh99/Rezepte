828d09e99b044c9750d0293f3c2b0a3b4202e57a
jegh99 <jan.gohlke2@web.de>
2026-02-06 14:52:22 +0100

    Groundfunctionality implemented.

--- sources/general_headers/Platform.h
+++ sources/general_headers/Platform.h
@@ -3,3 +3,18 @@
 // Author:      J. Gohlke
 // Description: general includes
 //----------------------------------------------------------------------------------------
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+//----------------------------------------------------------------------------------------
+//                                      Incldues
+//----------------------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <Types.h>
+#include <Config.h>
+
+#endif
--- sources/general_headers/Types.h
+++ sources/general_headers/Types.h
@@ -3,3 +3,32 @@
 // Author:      J. Gohlke
 // Description: Typedefinitions
 //----------------------------------------------------------------------------------------
+#ifndef TYPES_H
+#define TYPES_H
+
+//----------------------------------------------------------------------------------------
+//                                      Incldues
+//----------------------------------------------------------------------------------------
+#include <stdbool.h>
+#include <stdint.h>
+
+
+
+
+
+//----------------------------------------------------------------------------------------
+//                                      Defines
+//----------------------------------------------------------------------------------------
+#define BOOLEAN bool
+#define CHAR    char
+#define INT8    int8_t
+#define INT16   int16_t
+#define INT32   int32_t
+#define INT64   int64_t
+
+#define UINT8   uint8_t
+#define UINT16  uint16_t
+#define UINT32  uint32_t
+#define UINT64  uint64_t
+
+#endif
