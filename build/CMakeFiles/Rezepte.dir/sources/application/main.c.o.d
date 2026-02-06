79b8513f6a7aefcbcc62f2626936cd616bdbdaf9
jegh99 <jan.gohlke2@web.de>
2026-02-06 15:34:57 +0100

    Implemented first file programm.

--- build/CMakeFiles/Rezepte.dir/sources/application/main.c.o.d
+++ build/CMakeFiles/Rezepte.dir/sources/application/main.c.o.d
@@ -30,6 +30,7 @@ CMakeFiles/Rezepte.dir/sources/application/main.c.o: \
  /usr/lib/gcc/x86_64-linux-gnu/11/include/stdint.h /usr/include/stdint.h \
  /usr/include/x86_64-linux-gnu/bits/wchar.h \
  /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
- /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h /usr/include/string.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h /usr/include/string.h \
  /home/jeg/projects/Rezepte/sources/general_headers/Types.h \
  /home/jeg/projects/Rezepte/sources/general_headers/Config.h
