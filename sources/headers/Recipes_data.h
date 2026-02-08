b9db5d1834830591a3c337eb0176229341cde59f
jegh99 <jan.gohlke2@web.de>
2026-02-08 15:15:46 +0100

    Added Recipes.c and Recipes_data.h.

---
+++ sources/headers/Recipes_data.h
@@ -0,0 +1,9 @@
+//----------------------------------------------------------------------------------------
+// Title:       Recipes_data.h
+// Author:      J. Gohlke
+// Description: Headerfile for Recipes Datas
+//----------------------------------------------------------------------------------------
+#ifndef RECIPES_DATA_H
+#define RECIPES_DATA_H
+
+#endif
